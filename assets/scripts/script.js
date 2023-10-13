79bf4b89cce0351e8b65155797761bc3a15931e1
Arinze999 <akanagapyt@gmail.com>
2023-10-13 16:08:58 +0100

    new file:   assets/img/bg-buildings.jpg 	new file:   assets/img/bg-tireTrack.jpg 	new file:   assets/img/car-bannerHeader.png 	new file:   assets/img/car-fleet-images/VW-passat.png 	new file:   assets/img/car-fleet-images/audi.png 	new file:   assets/img/car-fleet-images/honda.png 	new file:   assets/img/car-fleet-images/lexus.png 	new file:   assets/img/car-fleet-images/mercedes-benz-GLK-2.png 	new file:   assets/img/car-fleet-images/toyota-corola.png 	new file:   assets/img/car-fleet-images/toyota2-removebg-preview.png 	new file:   assets/img/fordCar-imgBanner-flipped.png 	new file:   assets/img/hero-bg.3b5f7a2502f0f81d1490.png 	new file:   assets/img/icons8-car-60.png 	new file:   assets/img/icons8-car-64 (2).png 	new file:   assets/img/icons8-car-64 (3).png 	new file:   assets/img/icons8-car-cleaning-64.png 	new file:   assets/img/icons8-car-service-64.png 	new file:   assets/img/icons8-online-support-64.png 	new file:   assets/scripts/script.js 	new file:   assets/styles/style.css 	new file:   index.html

---
+++ assets/scripts/script.js
@@ -0,0 +1,110 @@
+// navbar
+const navBar = document.getElementById("d-navigate");
+const navBtn = document.getElementById("nav-open");
+
+// rental fleet elements
+const rough = document.getElementById("car-list").getElementsByTagName("li");
+const carList = Array.from(rough);
+const roughTd = document
+  .getElementById("car-details-table")
+  .getElementsByTagName("td");
+const carDetails = Array.from(roughTd);
+const toPay = document.getElementById("price");
+const carImage = document.getElementById("car-select");
+
+// car details array of objects
+const carListDetails = [
+  {
+    rent: "#40000 ",
+    carName: "Mercedes Benz GLK",
+    model: "Benz GLK",
+    mark: "Mercedes",
+    year: "2006",
+    doors: "4/5",
+    ac: "Yes",
+    transmission: "Manual",
+    fuel: "Diesel",
+    src: "assets/img/car-fleet-images/mercedes-benz-GLK-2.png",
+  },
+  {
+    rent: "#21500 ",
+    carName: "VW Passat",
+    model: "Passat",
+    mark: "Volkswagen",
+    year: "2008",
+    doors: "4/5",
+    ac: "Yes",
+    transmission: "Manual",
+    fuel: "Gasoline",
+    src: "assets/img/car-fleet-images/VW-passat.png",
+  },
+  {
+    rent: "#20000 ",
+    carName: "Toyota Camry",
+    model: "Camry",
+    mark: "Toyota",
+    year: "2010",
+    doors: "4/5",
+    ac: "Yes",
+    transmission: "Dual Support",
+    fuel: "Hybrid",
+    src: "assets/img/car-fleet-images/toyota2-removebg-preview.png",
+  },
+  {
+    rent: "#35000 ",
+    carName: "Lexus ES",
+    model: "ES 350",
+    mark: "Lexus",
+    year: "2020",
+    doors: "4/5",
+    ac: "Yes",
+    transmission: "Automatic",
+    fuel: "Gasoline",
+    src: "assets/img/car-fleet-images/lexus.png",
+  },
+  {
+    rent: "#27400 ",
+    carName: "Honda CR-V",
+    model: "CR-V",
+    mark: "Honda",
+    year: "2016",
+    doors: "4/5",
+    ac: "Yes",
+    transmission: "Dual Support",
+    fuel: "Diesel",
+    src: "assets/img/car-fleet-images/honda.png",
+  },
+  {
+    rent: "#23000 ",
+    carName: "Audi A-6",
+    model: "A-6",
+    mark: "Audi",
+    year: "2016",
+    doors: "4/5",
+    ac: "Yes",
+    transmission: "Automatic",
+    fuel: "Hybrid",
+    src: "assets/img/car-fleet-images/audi.png",
+  },
+];
+
+// function for clicking on each carList item
+carList.map((each) => {
+  each.addEventListener("click", () => {
+    toPay.innerText = carListDetails[carList.indexOf(each)].rent;
+    carImage.setAttribute("src", carListDetails[carList.indexOf(each)].src);
+    carDetails[0].innerText = carListDetails[carList.indexOf(each)].model;
+    carDetails[1].innerText = carListDetails[carList.indexOf(each)].mark;
+    carDetails[2].innerText = carListDetails[carList.indexOf(each)].year;
+    carDetails[3].innerText = carListDetails[carList.indexOf(each)].doors;
+    carDetails[4].innerText = carListDetails[carList.indexOf(each)].ac;
+    carDetails[5].innerText =
+      carListDetails[carList.indexOf(each)].transmission;
+    carDetails[6].innerText = carListDetails[carList.indexOf(each)].fuel;
+  });
+});
+
+//navbar responsive function
+navBtn.addEventListener("click", () => {
+  navBar.classList.toggle("show-nav");
+});
